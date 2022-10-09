64c42d41b894fb04206108fd2f85b40d92fa8a52
Mohanraj K <mohanraj.kandasamy@kohler.com>
2022-10-09 21:00:41 +0530

    Text field core component

--- src/components/organisms/organizations/Organizations.tsx
+++ src/components/organisms/organizations/Organizations.tsx
@@ -1,8 +1,22 @@
+import Input from "components/atoms/TextField";
+import SearchIcon from '../../../assets/images/search.svg';
 import { FC } from "react";
 
 const Organizations: FC = () => {
   return (
-    <h2>Organizations</h2>
+    <>
+      <h2>Add Organization</h2>
+      <div>
+        <Input 
+          inputId="name" 
+          value='' 
+          onChange={() => {}}
+          placeholder="Enter Name"
+          required
+          preFixIcon={SearchIcon}
+          message='Eg: Example'  />
+      </div>
+    </>
   )
 }
 export default Organizations;
